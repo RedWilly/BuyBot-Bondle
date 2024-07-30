aefa38a74739503609b2ecd58435a7592f5f25a5
RedWilly <thompsonau023@gmail.com>
2024-07-30 15:29:03 +0000

    Remove

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,3 @@
-// import dotenv from 'dotenv';
-// import { setupBlockchainListeners } from './blockchain';
-// import {
-//   sendTokenCreatedNotification,
-//   sendTokenBuyNotification,
-//   sendTokenSellNotification,
-// } from './bot';
-
-// dotenv.config();
-
-// console.log('Starting Token Monitor Telegram Bot...');
-
-// setupBlockchainListeners(
-//   sendTokenCreatedNotification,
-//   sendTokenBuyNotification,
-//   sendTokenSellNotification
-// );
-
-// console.log('Blockchain listeners set up. Waiting for events...');
 import dotenv from 'dotenv';
 import { setupBlockchainListeners } from './blockchain';
 import {
@@ -24,28 +5,15 @@ import {
   sendTokenBuyNotification,
   sendTokenSellNotification,
 } from './bot';
-import express from 'express';
 
 dotenv.config();
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+console.log('Starting Token Monitor Telegram Bot...');
 
-// Simple route for health check
-app.get('/', (req, res) => {
-  res.send('Token Monitor Telegram Bot is running!');
-});
+setupBlockchainListeners(
+  sendTokenCreatedNotification,
+  sendTokenBuyNotification,
+  sendTokenSellNotification
+);
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-  console.log('Starting Token Monitor Telegram Bot...');
-
-  setupBlockchainListeners(
-    sendTokenCreatedNotification,
-    sendTokenBuyNotification,
-    sendTokenSellNotification
-  );
-
-  console.log('Blockchain listeners set up. Waiting for events...');
-});
+console.log('Blockchain listeners set up. Waiting for events...');
